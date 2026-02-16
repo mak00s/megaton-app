4add37ae17b5ae401cd6a281b418d329148cd7cb
maks <mak@fish.razor.jp>
2026-02-16 21:43:07 +0900

    Refactor SLQM analysis to shared GA4 helper utilities

--- megaton_lib/__init__.py
+++ megaton_lib/__init__.py
@@ -4,6 +4,7 @@ Public modules
 --------------
 Core:
     megaton_client   : get_ga4() factory for Megaton instances
+    ga4_helpers      : run_report_df, build_filter, to_datetime_col, to_numeric_cols
     date_template    : resolve_date() for parameter date strings
     sheets           : save_sheet_from_template() and sheet utilities
 
