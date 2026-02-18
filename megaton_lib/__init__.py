c70cd51ba6ff11804a6d024f5b50c9eeba1a2952
maks <mak@fish.razor.jp>
2026-02-19 00:24:12 +0900

    chore: remove client-specific modules from public repository

--- megaton_lib/__init__.py
+++ megaton_lib/__init__.py
@@ -10,23 +10,4 @@ Core:
 
 Date utilities:
     date_utils       : month_ranges_for_year, month_ranges_between, months_between
-
-WITH report:
-    articles         : aggregate_article_meta()
-    with_report      : write_summary_sheets()
-
-Corp Talks report:
-    talks_scraping   : scrape_talk_cards, scrape_article_links, normalize_url, crawl_new_article_links
-    talks_ga4        : preprocess_page_metrics, preprocess_top_pages, fetch_nav_clicks, attach_nav_metrics
-    talks_report     : build_article_sheet, build_talks_m, build_monthly_rows, write_monthly_sheet
-    talks_retention  : query_retention_summary, query_retention_day_curve, query_retention_depth
-
-DEI Lab report:
-    dei_ga4          : classify_source_channel, ensure_trailing_slash, build_page_metrics
-
-SLQM report:
-    slqm_ga4         : get_13month_start, ym_from_year_month, safe_merge_many, fillna_int, compute_sp_ratio
-    slqm_analysis    : fetch_daily_metrics, fetch_page_metrics, fetch_channel_breakdown,
-                       fetch_source_medium, fetch_landing_pages, fetch_session_quality,
-                       fetch_new_vs_returning, fetch_page_transitions
 """
